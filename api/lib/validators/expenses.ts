436c528daaeddcccfc51fc85f693864c3b939a1a
orzarhi <or.zarhi11@gmail.com>
2024-05-19 11:19:14 +0300

    chore: Refactor expenses API to use typed schema

--- api/lib/validators/expenses.ts
+++ api/lib/validators/expenses.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
-export const createExpenseSchema = z.object({
+export const expensesSchema = z.object({
+    id: z.number().int().positive().min(1),
     title: z.string().min(3).max(100),
     amount: z.number().int().positive(),
 })
+
+export type Expense = z.infer<typeof expensesSchema>
+
+export const createExpenseSchema = expensesSchema.omit({ id: true })
--- api/routes/expenses.ts
+++ api/routes/expenses.ts
@@ -1,17 +1,11 @@
 import { Hono } from "hono";
 import { zValidator } from '@hono/zod-validator'
-import { createExpenseSchema } from "../lib/validators/expenses";
-
-type Expense = {
-    id: string
-    title: string
-    amount: number
-}
+import { createExpenseSchema, type Expense } from "../lib/validators/expenses";
 
 const fakeExpenses: Expense[] = [
-    { id: '1', title: 'Car Insurance', amount: 294.67 },
-    { id: '2', title: 'Rent', amount: 1000 },
-    { id: '3', title: 'Groceries', amount: 250.45 },
+    { id: 1, title: 'Car Insurance', amount: 294.67 },
+    { id: 2, title: 'Rent', amount: 1000 },
+    { id: 3, title: 'Groceries', amount: 250.45 },
 
 ]
 
@@ -21,7 +15,30 @@ expenses.get('/', (c) => c.json({ expenses: fakeExpenses }))
 
 expenses.post('/', zValidator("json", createExpenseSchema), (c) => {
     const expense = c.req.valid("json")
-    fakeExpenses.push({ id: (fakeExpenses.length + 1).toString(), ...expense })
-    return c.json(fakeExpenses)
+    fakeExpenses.push({ id: fakeExpenses.length + 1, ...expense })
+    return c.json(fakeExpenses, 201)
 })
 
+expenses.get('/:id{[0-9]+}', (c) => {
+    const id = +c.req.param("id")
+    const expense = fakeExpenses.find(e => e.id === id)
+
+    if (!expense) {
+        return c.notFound()
+    }
+
+    return c.json({ expense })
+
+})
+
+expenses.delete('/:id{[0-9]+}', (c) => {
+    const id = +c.req.param("id")
+    const index = fakeExpenses.findIndex(e => e.id === id)
+
+    if (index === -1) {
+        return c.notFound()
+    }
+
+    fakeExpenses.splice(index, 1)
+    return c.json(fakeExpenses)
+})

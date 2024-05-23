7f3c90a90d2b96ed1102f3297f9398bd5bcc142d
orzarhi <or.zarhi11@gmail.com>
2024-05-23 14:52:11 +0300

    chore: Update expenses API to calculate total spent and add Create Expense and Expenses routes

--- www/src/routes/index.tsx
+++ www/src/routes/index.tsx
@@ -1,13 +1,43 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/api";
+
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
-function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+async function getTotalSpent() {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  const data = await res.json();
+  return data;
 }
+
+function Index() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["total-spent"],
+    queryFn: getTotalSpent,
+  })
+
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The Total amount you've spent</CardDescription>
+      </CardHeader>
+      <CardContent>{isLoading ? '...' : data?.total}</CardContent>
+    </Card>
+  );
+};
